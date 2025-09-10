5e5855b2f7c8caf5469cdadc72bfb8d6ea6fbbfd
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-09-10 13:33:11 +0300

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld;
+    
+    /// <summary>
+    /// My first C# Program!
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, World!");
+            Console.WriteLine();
+            
+            /*
+             * Alternative output formats 
+             */
+            
+            string s = "World";
+            Console.WriteLine("Hello, {0}!", s); // Placeholder
+            
+            Console.WriteLine($"Hello, {s}!"); // Interpolation
+        }
+    }
