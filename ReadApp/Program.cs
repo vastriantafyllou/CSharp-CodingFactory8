d1ffa129182d7c65faf3c5ac4b917727df2a4061
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-09-10 19:43:57 +0300

    feat: add I/O

---
+++ ReadApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ReadApp;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float f = 0.0f;
+            double d = 0d;
+
+            Console.WriteLine("Παρακαλώ εισάγετε δύο δεκαδικούς");
+            f = float.Parse(Console.ReadLine()!);
+            d = double.Parse(Console.ReadLine()!);
+            
+            Console.WriteLine($"f = {f:N2}\td = {d:N2}");
+        }
+    }
---
+++ WriteApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace WriteApp;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Δήλωση και αρχικοποίηση μεταβλητών
+            int num1 = 10_000_000;
+            int num2 = 20_000_000;
+            int result = 0;
+            
+            // Εντολές 
+            result = num1 + num2;
+            
+            // Εκτύπωση αποτελεσμάτων
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            
+            Console.WriteLine("Το άθροισμα των " + num1 + " και " + num2 + " είναι: " + result);
+            Console.WriteLine("Το άθροισμα των {0,6:C2} και {1,6:N2} είναι: {2,10:N2}", num1, num2, result);
+            Console.WriteLine($"Το άθροισμα των {num1, 6:N2} και {num2, 6:N2} είναι: {result,10:N2}");
+        }
+    }
